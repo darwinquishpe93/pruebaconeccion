cad735fd12921cd5c6a09b1de83711f8d225ce8a
darwin <darwinquishpe93@hotmail.com>
2018-05-16 18:53:26 -0500

    pruebadb1

---
+++ conn.py
@@ -0,0 +1,12 @@
+from peewee import *
+    db = SqliteDatabase ('pruebadb')
+
+    class Student (Model)
+        username = Charfield(maxlength=255, unique=true)
+        points = integerfield(default =0 )
+
+    Class Meta:
+        database = db
+    if __name__ = "__main__":
+        db.connect()
+        db.create_table([Student], safe = true)   
